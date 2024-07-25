ed2b64a647605822baeab604c08df6123e9c7877
Jacob <jacob.roling@hey.com>
2024-07-25 20:10:04 +0930

    chore: savepoint

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("node:dns");
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  (req, _, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+  },
+  cors(),
+  bodyParser.urlencoded({ extended: false })
+);
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urls = [];
+
+app.post("/api/shorturl", function (req, res) {
+  try {
+    const url = new URL(req.body.url);
+    if (!(url.protocol == "http:" || url.protocol == "https:")) {
+      return res.json({
+        error: "invalid url",
+      });
+    }
+    urls.push(url);
+    return res.json({ original_url: url.href, short_url: urls.length - 1 });
+  } catch (err) {
+    return res.json({
+      error: "invalid url",
+    });
+  }
+  // dns.lookup(url.host, (err, address, family) => {
+  //   if (err)
+  //     return res.json({
+  //       error: "invalid url",
+  //     });
+  // });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:url", function (req, res) {
+  // console.log(req.params.url);
+  if (urls[req.params.url]) {
+    return res.redirect(urls[req.params.url]);
+  } else {
+    return res.json({
+      error: "invalid url",
+    });
+  }
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
